e77f180cefcf0e4caeae83658628a6aef290a581
WeRockStar <vr_freestyle@outlook.com>
2016-04-16 16:38:04 +0700

    Custom TextView

--- app/src/main/java/com/werockstar/redditreader/view/custom/CustomTextView.java
+++ app/src/main/java/com/werockstar/redditreader/view/custom/CustomTextView.java
@@ -1,4 +1,38 @@
 package com.werockstar.redditreader.view.custom;
 
-public class CustomTextView {
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.werockstar.redditreader.manager.Contextor;
+
+public class CustomTextView extends TextView {
+
+    public CustomTextView(Context context) {
+        super(context);
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public CustomTextView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public void init() {
+        Typeface typeface =
+                Typeface.createFromAsset(
+                        Contextor.getInstance().getContext().getAssets(), "fonts/thaisans.ttf"
+                );
+        setTypeface(typeface);
+    }
 }
