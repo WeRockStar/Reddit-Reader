5d940f5dd99f217e6e9d62bfe39aee4651cb8c7b
WeRockStar <vr_freestyle@outlook.com>
2016-04-23 12:10:07 +0700

    Removed addToBackStack null

--- app/src/main/java/com/werockstar/redditreader/view/activity/MainActivity.java
+++ app/src/main/java/com/werockstar/redditreader/view/activity/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.contentContainer, fragment)
-                .addToBackStack(null)
                 .commit();
 
     }
